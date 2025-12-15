7dd469bb8fc16dc29c3948e89a8fb0d42e89b7a0
sobolm20 <matiassobol20@gmail.com>
2025-12-15 19:07:26 -0300

    entregable

--- src/pages/Detalles.jsx
+++ src/pages/Detalles.jsx
@@ -1,11 +1,11 @@
-import Wines from '../components/ProductList'
+import CrudProducts from '../components/CrudProducts'
 
 export default function Filtrado() {
 
     return(
         <div className="container py-4">
             <h1 className="text-center mb-4">Vino Seleccionado</h1>
-            <Wines />
+            <CrudProducts />
         </div>
 
     )
